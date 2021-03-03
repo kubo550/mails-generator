1d8d9c2537aaf33e8ed6c882c4807c536afc1f02
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-03-03 22:23:23 +0100

    Dist javascript

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,79 @@
 import puppeteer from "puppeteer";
+// @ts-ignore
+import fng from "fakenamegenerator";
 import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 const FAKE_MAIL_URL = process.env.FAKE_MAIL_URL;
 const MAX_LENGTH = +process.env.MAX_LENGTH;
-let emails = [];
-const getFakeMail = async () => {
+let accounts = [];
+const getFakeMail = async (fakePerson) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(FAKE_MAIL_URL, { waitUntil: "networkidle2" });
-    await page.waitForTimeout(4000);
-    const emial = await page.$eval("#mail", (el) => el.value);
-    emails.push(emial);
-    if (emails.length === MAX_LENGTH) {
-        emails = emails.filter(e => e !== "Ładowanie...");
-        fs.writeFile(`./fakeEmails/${emails[0].split(/[0-9@]/)[0]}.json`, JSON.stringify(emails, null, 3), err => {
-            err && console.log(err);
-        });
-    }
+    const closeButton = await page.$(".close-x");
+    await closeButton?.click();
+    const name = await page.$("#name");
+    const surname = await page.$("#surname");
+    const birthdayDay = await page.$("#birthdayDay");
+    const month = await page.$(".account-input");
+    const birthdayYear = await page.$("#birthdayYear");
+    const login = await page.$("#login");
+    const password = await page.$("#password");
+    const rePassword = await page.$("#rePassword");
+    const regulamin = await page.$(".law-information__description");
+    const _fullnameArr = fakePerson.name.split(/\s+/);
+    const userData = {
+        name: _fullnameArr[0],
+        surname: _fullnameArr[_fullnameArr.length - 1],
+        password: fakePerson.password,
+        birthdayDay: fakePerson.birthday.split("/")[0],
+        birthdayYear: fakePerson.birthday.split("/")[2],
+    };
+    await name?.type(userData.name);
+    await surname?.type(userData.surname);
+    await birthdayDay?.type(userData.birthdayDay);
+    await month?.click();
+    await password?.type(userData.password);
+    await rePassword?.type(userData.password);
+    await login?.click();
+    await page.waitForTimeout(300);
+    await regulamin?.click();
+    await page.waitForTimeout(200);
+    const monthJanuary = await page.$(".account-select");
+    await monthJanuary?.click();
+    await page.waitForTimeout(200);
+    await page.keyboard.press("ArrowDown");
+    await page.keyboard.press("Enter");
+    await birthdayYear?.type(userData.birthdayYear);
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("ArrowDown");
+    await page.keyboard.press("Enter");
+    const accountLogin = await page.$eval("#login", input => input.value);
+    const res = {
+        fullname: `${userData.name} ${userData.surname}`,
+        email: `${accountLogin}@interia.pl`,
+        password: userData.password,
+    };
+    console.log("inside", res);
+    const submit = await page.$(".btn");
+    await submit?.click();
+    await page.waitForTimeout(4500);
     await browser.close();
+    return res;
 };
-fs.mkdir(`./fakeemails`, { recursive: true }, err => {
+fs.mkdir(`./fakeAccounts`, { recursive: true }, err => {
     err && console.log(err);
 });
 for (let i = 0; i < MAX_LENGTH; i++) {
-    getFakeMail();
+    (async () => {
+        const fakePerson = await fng();
+        const data = await getFakeMail(fakePerson);
+        accounts.push(data);
+        if (accounts.length === MAX_LENGTH) {
+            fs.writeFile(`./fakeAccounts/${data.fullname}.json`, JSON.stringify(accounts, null, 3), err => {
+                err && console.log(err);
+            });
+        }
+    })();
 }
