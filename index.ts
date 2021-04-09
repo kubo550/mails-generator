61064fda936b57827917d8c7354b748145ba99e2
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-04-10 00:42:55 +0200

    create new index

--- index.ts
+++ index.ts
@@ -1,111 +1,159 @@
 import puppeteer from "puppeteer";
+import dotenv from "dotenv";
+import chalk from "chalk";
 // @ts-ignore
 import fng from "fakenamegenerator";
-import fs from "fs";
-import dotenv from "dotenv";
-import { FakePerson } from "./types/types";
+
+import { Person } from "./types";
 
 dotenv.config();
 
-const FAKE_MAIL_URL = process.env.FAKE_MAIL_URL!;
-const MAX_LENGTH = +process.env.MAX_LENGTH!;
 
-let accounts = [];
+const password = process.env.PASSWORD!;
+const timeoutMS = Number(process.env.TIMEOUT_MS!);
 
-const getFakeMail = async (fakePerson: FakePerson) => {
-    const browser = await puppeteer.launch();
+const URL = "https://konto-pocztowe.interia.pl/#/nowe-konto/darmowe";
+const DOMAIN = "@interia.pl";
+let trials = 0;
+let created = 0;
+
+const getSafeDay = (day: string): string => (day === "31" ? "30" : day);
+
+const random = (limit: number): number => Math.ceil(Math.random() * limit);
+
+const createAccount = async (
+    person: Person,
+    password: string
+): Promise<[string, boolean]> => {
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto(FAKE_MAIL_URL, { waitUntil: "networkidle2" });
 
-    fs.unlinkSync('./dist/index.js')
+    const { name, birthday } = person;
+    const names = name.split(/\s+/);
+    const dates = birthday.split("/");
+
+    const imie = names[0];
+    const surname = names[names.length - 1];
+
+    const day = dates[0];
+    const year = dates[dates.length - 1];
+
+    if (!imie || !surname || !day || !year) {
+        throw Error("Fng failed");
+    }
+
+    await page.goto(URL, { waitUntil: "networkidle2" });
+    await page.waitForTimeout(500);
 
     const closeButton = await page.$(".close-x");
-
     await closeButton?.click();
 
-    const name = await page.$("#name");
-    const surname = await page.$("#surname");
-    const birthdayDay = await page.$("#birthdayDay");
-    const month = await page.$(".account-input");
-    const birthdayYear = await page.$("#birthdayYear");
-    const login = await page.$("#login");
+    await page.waitForTimeout(timeoutMS);
 
-    const password = await page.$("#password");
-    const rePassword = await page.$("#rePassword");
-    const regulamin = await page.$(".law-information__description");
-
-    const _fullnameArr = fakePerson.name.split(/\s+/);
-
-    const userData = {
-        name: _fullnameArr[0],
-        surname: _fullnameArr[_fullnameArr.length - 1],
-        password: fakePerson.password,
-        birthdayDay: fakePerson.birthday.split("/")[0],
-        birthdayYear: fakePerson.birthday.split("/")[2],
-    };
-
-    await name?.type(userData.name);
-    await surname?.type(userData.surname);
-    await birthdayDay?.type(userData.birthdayDay);
-    await month?.click();
-
-    await password?.type(userData.password);
-    await rePassword?.type(userData.password);
-    await login?.click();
-    await page.waitForTimeout(300);
-    await regulamin?.click();
-
-    await page.waitForTimeout(200);
-    const monthJanuary = await page.$(".account-select");
-
-    await monthJanuary?.click();
-    await page.waitForTimeout(200);
-
-    await page.keyboard.press("ArrowDown");
-    await page.keyboard.press("Enter");
-
-    await birthdayYear?.type(userData.birthdayYear);
+    const imieInput = await page.$("#name");
+    await imieInput?.click();
+    await page.keyboard.type(imie, { delay: 10 });
 
     await page.keyboard.press("Tab");
-    await page.keyboard.press("ArrowDown");
+    await page.waitForTimeout(timeoutMS);
+    await page.keyboard.type(surname, { delay: 10 });
+    await page.waitForTimeout(timeoutMS);
+
+    await page.keyboard.press("Tab");
+    await page.keyboard.type(getSafeDay(day), { delay: 10 });
+    await page.waitForTimeout(timeoutMS);
+
+    await page.keyboard.press("Tab");
+    const r1 = random(12);
+
+    for (let i = 0; i < r1; i++) {
+        await page.keyboard.press("ArrowDown");
+    }
+
     await page.keyboard.press("Enter");
+    await page.waitForTimeout(timeoutMS);
 
-    const accountLogin = await page.$eval(
-        "#login",
-        input => (input as HTMLInputElement).value
-    );
+    await page.keyboard.press("Tab");
+    await page.keyboard.type(year, { delay: 10 });
+    await page.waitForTimeout(timeoutMS);
 
-    const res = {
-        fullname: `${userData.name} ${userData.surname}`,
-        email: `${accountLogin}@interia.pl`,
-        password: userData.password,
-    };
+    await page.keyboard.press("Tab");
+    const r2 = random(2);
 
-    const submit = await page.$(".btn");
-    await submit?.click();
-    await page.waitForTimeout(4500);
+    for (let i = 0; i < r2; i++) {
+        await page.keyboard.press("ArrowDown");
+    }
+
+    await page.keyboard.press("Enter");
+    await page.waitForTimeout(timeoutMS);
+
+    await page.keyboard.press("Tab");
+    await page.waitForTimeout(timeoutMS);
+
+    const passwordInput = await page.$("#password");
+    await passwordInput?.click();
+    await page.keyboard.type(password, { delay: 10 });
+    await page.waitForTimeout(timeoutMS);
+
+    const rePasswordInput = await page.$("#rePassword");
+    await rePasswordInput?.click();
+    await page.keyboard.type(password, { delay: 10 });
+    await page.waitForTimeout(timeoutMS);
+
+    const loginInput = await page.$("#login");
+    const loginEl = await loginInput?.getProperty("value");
+    const login = loginEl?._remoteObject.value as string;
+    await page.waitForTimeout(timeoutMS);
+
+    // const checkbox = await page.$('.checkbox-label')
+    // await checkbox?.click()
+    await page.waitForTimeout(timeoutMS);
+
+    const errors = await page.$(".account-input__error");
+
+    const registerBtn = await page.$(".btn");
+    await registerBtn?.click();
+    await page.waitForTimeout(timeoutMS * 2);
+
     await browser.close();
 
-    return res;
+    const mail = login + DOMAIN;
+    const completed = !errors;
+    return [mail, completed];
 };
 
-fs.mkdir(`./fakeAccounts`, { recursive: true }, err => {
-    err && console.log(err);
-});
+const interval = async (): Promise<void> => {
+    const person: Person = await fng();
 
-for (let i = 0; i < MAX_LENGTH; i++) {
-    (async () => {
-        const fakePerson: FakePerson = await fng();
-        const data = await getFakeMail(fakePerson);
-        accounts.push(data);
-        if (accounts.length === MAX_LENGTH) {
-            fs.writeFile(
-                `./fakeAccounts/${data.fullname}.json`,
-                JSON.stringify(accounts, null, 3),
-                err => {
-                    err && console.log(err);
-                }
-            );
-        }
-    })();
-}
+    const [mail, completed] = await createAccount(person, password);
+    trials++;
+    if (completed) {
+        created += 1;
+    }
+
+    console.log(
+        chalk.blue(mail),
+        chalk.magenta(password),
+        chalk[completed ? "green" : "red"](completed),
+        `${chalk.greenBright(trials)}/${chalk.cyanBright(trials)}`
+    );
+
+    await interval();
+};
+
+(async () => {
+    console.log(chalk.cyanBright("Start creating..."));
+    try {
+        await interval();
+    } catch (err) {
+        console.log(
+            chalk.yellowBright`Something went really wrong!`,
+            chalk.red(err)
+        );
+    } finally {
+        console.log(
+            `Total trails: ${chalk.blue(trials)}`,
+            `Total created: ${chalk.green(created)}`
+        );
+    }
+})();
